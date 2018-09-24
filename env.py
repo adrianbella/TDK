7351970c14f66d7e3f2e0291730618c9ee1b10f9
adrian <bella.adus@gmail.com>
2018-09-25 01:39:47 +0200

    finishing environment initialization part and implementing movements in the virtual env

--- env.py
+++ env.py
@@ -1,3 +1,4 @@
+import random
 import imageio
 import glob
 import cv2
@@ -6,22 +7,28 @@ import math
 
 
 class Environment:
-    all_screenShots_path = "./Screenshots/*/*.png"
-    test_screenShots_path = "./Screenshots/andromeda/*.png"
-    img_array = np.zeros(6615, dtype=[('name', 'U10'), ('radius', 'f4'), ('fi_angel', 'f4'), ('theta_angel', 'f4'),
-                                      ('img', 'i4', (200, 200))])
-    fi_array = np.zeros(100, dtype=int)
+    traning_names = np.array(
+        ['andromeda', 'assistant', 'dreyar', 'eve', 'jasper', 'kachujin', 'liam', 'lola', 'malcolm', 'mark', 'medea',
+         'peasant'])
+    validation_names = np.array(['regina', 'remy', 'stefani'], dtype='U10')
+
+    img_array = np.zeros((7, 45, 21, 200, 200))
+
+    current_name = ''
+    current_state = np.zeros(3)
+
+    actions = np.array(['forward', 'backward', 'up', 'down', 'left', 'right'])
 
     def __init__(self):
         self.read_files()
 
     def read_files(self):
-        idx = 0
-        for im_path in glob.glob(self.test_screenShots_path):
+
+        self.current_name = self.get_random_name()
+        current_figure_screenShots_path = "./Screenshots/" + self.current_name + "/*.png"
+
+        for im_path in glob.glob(current_figure_screenShots_path):
             img = imageio.imread(im_path)
-
-            name = self.get_name(im_path)
-
             gray_scale_img = self.gray_scale(img)
 
             x, y, z = self.get_descartes_coordinates(im_path)
@@ -31,18 +38,12 @@ class Environment:
             fi_index = int(fi / 8)  # fi_index [0:44]
             theta_index = int((theta - 10) / 8)  # theta_index [0:20]
 
-            print(r, r_index, fi, fi_index, theta, theta_index)
+            self.img_array[r_index, fi_index, theta_index] = gray_scale_img
 
-            element = np.array([(name, r, fi_index, theta_index, gray_scale_img)],
-                               dtype=[('name', 'U10'), ('radius_index', 'i2'), ('fi_angel_index', 'i2'), ('theta_angel_index', 'i2'),
-                                      ('img', 'i4', (200, 200))])
-
-            self.img_array[idx] = element
-            idx += 1
-
-    def get_name(self, img_path):
-        first_cut = img_path[14:]
-        name = first_cut.split('/')[0]
+    def get_random_name(self):
+        figure_index = random.randint(0, len(self.traning_names) - 1)
+        name = self.traning_names[figure_index]
+        self.traning_names = np.delete(self.traning_names, figure_index)
         return name
 
     def gray_scale(self, img):
@@ -71,7 +72,7 @@ class Environment:
         fi = int(round(fi))
         r = round(r, 2)
 
-        # convert fi to [0:360] intervall
+        # convert fi to [0:360] interval
         if fi < 0:
             fi += 360
 
@@ -83,3 +84,64 @@ class Environment:
         # -------------------------------
 
         return r, fi, theta
+
+    def get_random_initial_state(self):
+        r_index = random.randint(0, 6)
+        fi_index = random.randint(0, 44)
+        theta_index = random.randint(0, 20)
+
+        self.current_state = np.array([r_index, fi_index, theta_index])
+
+        return self.img_array[r_index, fi_index, theta_index]
+
+    def step(self, action_index):
+        action = self.actions[action_index]
+
+        previous_state = self.current_state
+
+        r_index = self.current_state[0]
+        fi_index = self.current_state[1]
+        theta_index = self.current_state[2]
+
+        if action == 'forward':
+            if r_index != 0:  # check if we can move forward
+                r_index -= 1
+            print('forward')
+        elif action == 'backward':
+            if r_index != 6:  # check if we can move backward
+                r_index += 1
+            print('backward')
+        elif action == 'up':
+            if theta_index != 0:  # check if we can move up
+                theta_index -= 1
+            print('up')
+        elif action == 'down':
+            if theta_index != 20:  # check if we can move down
+                theta_index += 1
+            print('down')
+        elif action == 'left':
+            if fi_index == 0:  # we are across from the figure
+                fi_index = 44
+            else:  # all other cases
+                fi_index -= 1
+            print('left')
+        elif action == 'right':
+            if fi_index == 44:
+                fi_index = 0
+            else:
+                fi_index += 1
+            print('right')
+
+        self.current_state = np.array(
+            [r_index, fi_index, theta_index])  # save the polar coordinates of the current state
+
+        reward = self.get_reward(previous_state)  # calcuate the reward
+        observation = self.img_array[r_index, fi_index, theta_index]  # find the new camera input
+
+        print(previous_state)
+        print(self.current_state)
+
+        return reward, observation
+
+    def get_reward(self, prev_state):
+        return random.randint(0, 1)
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import env
+import random
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
 
-    env = env.Environment()  # 
-    print('haha')
+    env = env.Environment()  # environment initialization
+
+    observation = env.get_random_initial_state()
+
+    for i in range(0, 100):
+        reward, observation = env.step(random.randint(0, 5))
+
+    print('end')
